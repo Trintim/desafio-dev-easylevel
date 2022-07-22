954553ab3c778161796edd8e0a828b93a383c9c3
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2022-07-22 13:54:08 -0300

    commit

--- ADA.c
+++ ADA.c
@@ -10,31 +10,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void calculoSalario(double salario){
 
-    double salario;
     double novoSalario;
-
     printf("\nInforme o Salario do colaborador para reajuste: ");
     scanf("%lf", &salario);
 
-    if(salario > 0 && salario < 2000){
+        if(salario > 0 && salario < 2000){
         novoSalario = salario * 1.20;
-    }
+        }
 
-    if(salario >= 2000 && salario < 4000){
-        novoSalario = salario * 1.15;
-    }
+        if(salario >= 2000 && salario < 4000){
+            novoSalario = salario * 1.15;
+        }
 
-    if(salario >= 4000 && salario < 7000){
-        novoSalario = salario * 1.10;
-    }
+        if(salario >= 4000 && salario < 7000){
+            novoSalario = salario * 1.10;
+        }
 
-    if(salario >= 7000){
-        novoSalario = salario * 1.05;
-    }
+        if(salario >= 7000){
+            novoSalario = salario * 1.05;
+        }
 
     printf("\nReajuste salarial aplicado o novo salario e de: %.3lf", novoSalario);
+}
+
+int main(){
+
+    double pagamento;
+
+    calculoSalario(pagamento);
 
     return 0;
 }
